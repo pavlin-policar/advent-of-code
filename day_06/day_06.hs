39cb9f3e859e91d8e72c8991e8e1db7f314ea19c
Pavlin Poličar <pavlin.g.p@gmail.com>
2021-12-06 12:04:26 +0100

    Day 6: Solve pt. 2

--- day_06/day_06.hs
+++ day_06/day_06.hs
@@ -1,5 +1,7 @@
+import qualified Data.Map as M
 
-newtype Days = Days Int
+
+type StateCounts = M.Map Int Int
 
 
 -- Split string on delimiter
@@ -22,6 +24,23 @@ step state = resetAdults $ newState ++ (offspring state)
           resetAdults = map (\x -> if x < 0 then 6 else x)
 
 
+stateCounts :: [Int] -> StateCounts
+stateCounts state = M.fromListWith (+) (map (\k -> (k, 1)) state)
+
+
+stepEfficient :: StateCounts -> StateCounts
+stepEfficient state = M.fromListWith (+) $ ((resetAdults . decAdults) state) ++ offspring
+    where
+        decAdults = map (\(k, v) -> (k - 1, v)) . M.toList
+        resetAdults = map (\(k, v) -> if k < 0 then (6, v) else (k, v))
+        offspring = case M.lookup 0 state of
+            Nothing -> []
+            Just x -> [(8, x)]
+
+
 main = do
     initialState <- readData "input.txt"
-    putStrLn $ show $ length $ iterate step initialState !! 80
+    -- Part 1
+    --putStrLn $ show $ length $ iterate step initialState !! 80
+    -- Part 2
+    putStrLn $ show $ M.foldl (+) 0 $ iterate stepEfficient (stateCounts initialState) !! 256
