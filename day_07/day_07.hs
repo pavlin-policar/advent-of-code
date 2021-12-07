7ff156eecd50f0b8260abad5d1972708ed17744a
Pavlin Poličar <pavlin.g.p@gmail.com>
2021-12-07 10:02:12 +0100

    Day 7: Solve pt. 2

--- day_07/day_07.hs
+++ day_07/day_07.hs
@@ -15,8 +15,15 @@ findMinCost :: [Int] -> (Int, Int)
 findMinCost positions = foldl1 (\t0 t1 -> if snd t1 < snd t0 then t1 else t0) costs
     where
         candidatePositions = [(minimum positions) .. (maximum positions)]
-        costs = map (\p -> (p, sum (calculateCost p positions))) candidatePositions
-        calculateCost val = map (abs . subtract val)
+        costs = map (\t -> (t, sum (map (crabWalkCostLin t) positions))) candidatePositions
+
+
+crabWalkCostConst :: Int -> Int -> Int
+crabWalkCostConst to from = abs (from - to)
+
+
+crabWalkCostLin :: Int -> Int -> Int
+crabWalkCostLin to from = sum [1..(abs (from - to))]
 
 
 main = do
