4e12ea1141dc4e3e736f984b00e24d0a52d63c81
Pavlin Poličar <pavlin.g.p@gmail.com>
2021-12-16 17:41:47 +0100

    Day 10: Solve pt. 2

--- day_10/day_10.hs
+++ day_10/day_10.hs
@@ -2,6 +2,7 @@ import qualified Data.Map as Map
 import qualified Data.Set as Set
 import Data.Map ((!))
 import Data.Maybe (mapMaybe)
+import qualified Data.List as List
 
 readData fname = do
     contents <- readFile fname
@@ -9,24 +10,41 @@ readData fname = do
 
 
 errorScoreTable = Map.fromList [(')', 3), (']', 57), ('}', 1197), ('>', 25137)]
+autoCompleteScoreTable = Map.fromList [(')', 1), (']', 2), ('}', 3), ('>', 4)]
 
 pairs = Map.fromList [('(', ')'), ('[', ']'), ('{', '}'), ('<', '>')]
 
 
-isCorrupt :: String -> Maybe Char
-isCorrupt s = isCorrupt' s []
+data CharStatus = Ok | Illegal Char | Missing String deriving (Show)
+
+
+evalString :: String -> CharStatus
+evalString s = evalString' s []
     where
-        isCorrupt' [] stack = Nothing
-        isCorrupt' (x:xs) stack =
-            if Set.member x (Set.fromList ['(', '[', '{', '<']) then
-                isCorrupt' xs (pairs ! x : stack)
-            else
-                if head stack == x then
-                    isCorrupt' xs (tail stack)
-                else
-                    Just x
+        evalString' [] stack = if null stack then Ok else Missing stack
+        evalString' (x:xs) stack
+          | Set.member x (Set.fromList ['(', '[', '{', '<']) = evalString' xs (pairs ! x : stack)
+          | head stack == x = evalString' xs (tail stack)
+          | otherwise = Illegal x
+
+
+isIllegal :: CharStatus -> Bool
+isIllegal (Illegal _) = True
+isIllegal _ = False
+
+
+autoCompleteScore :: CharStatus -> Integer
+autoCompleteScore (Missing s) = foldl (\acc x -> acc * 5 + x) 0 numericScores
+    where numericScores = map (autoCompleteScoreTable !) s
+autoCompleteScore _ = error "Illegal call!"
 
 
 main = do
     lines <- readData "input.txt"
-    print $ sum $ map (errorScoreTable !) (mapMaybe isCorrupt lines)
+    let lineStats = map evalString lines
+    -- Part 1
+    print $ sum $ map (\(Illegal x) -> errorScoreTable ! x) (filter isIllegal lineStats)
+    -- Part 2
+    let filteredLines = filter (not . isIllegal) lineStats
+    let lineScores = map autoCompleteScore filteredLines
+    print $ List.sort lineScores !! (length lineScores `div` 2)
