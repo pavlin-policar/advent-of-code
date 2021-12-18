6c3e1d42095708a765cb1333fc362a286b3b7e66
Pavlin Poličar <pavlin.g.p@gmail.com>
2021-12-18 16:12:35 +0100

    Day 12: Refactor to count number of visits

--- day_12/day_12.hs
+++ day_12/day_12.hs
@@ -2,6 +2,7 @@ import qualified Data.Text as T
 import qualified Data.Map as M
 import qualified Data.Set as Set
 import qualified Data.Char as Char
+import Data.Map ((!))
 
 
 readGraph fname = do
@@ -17,22 +18,37 @@ edges2adj :: [(String, String)] -> AdjacencyList
 edges2adj edges = M.fromListWith (++) (map (\(x, y) -> (x, [y])) edges)
 
 
-generatePaths :: AdjacencyList -> String -> String -> [[String]]
-generatePaths graph source target = generatePaths' Set.empty [[]] source
-    where
-        generatePaths' :: Set.Set String -> [[String]] -> String -> [[String]]
-        generatePaths' disallowed paths source
-            | source == target = addToPaths paths source
-            | null children = [[]]
-            | otherwise = concatMap (generatePaths' newDisallowed newPaths) children
-            where
-                children = filter (\x -> not (Set.member x disallowed)) $ M.findWithDefault [] source graph
-                newDisallowed = if Char.isLower (head source) then Set.insert source disallowed else disallowed
-                newPaths = addToPaths paths source
+-- Keep track of number of times we visit nodes
+type StateDict =  M.Map String Int
+
+isAllowed :: StateDict -> String -> Bool
+isAllowed stateDict node
+    | node == "start" || node == "stop" = M.findWithDefault 0 node stateDict < 1
+    | Char.isUpper (head node) = True
+    | otherwise = M.findWithDefault 0 node stateDict < 1
+
+updateStateDict :: String -> StateDict -> StateDict
+updateStateDict node = M.insertWith (+) node 1
 
 
+-- Add a node to the end of a list of paths e.g. add 9 to [[1, 2], [3, 4]] -> [[1, 2, 9], [3, 4, 9]]
 addToPaths :: [[a]] -> a -> [[a]]
 addToPaths paths node = map (\t -> t ++ [node]) paths
+
+
+-- Generate paths through the graph
+generatePaths :: AdjacencyList -> String -> String -> [[String]]
+generatePaths graph source target = generatePaths' M.empty [[]] source
+    where
+        generatePaths' :: StateDict -> [[String]] -> String -> [[String]]
+        generatePaths' state paths source
+            | source == target = addToPaths paths source
+            | null children = [[]]
+            | otherwise = concatMap (generatePaths' (updateStateDict source state) newPaths) children
+            where
+                children = filter (isAllowed state) (M.findWithDefault [] source graph)
+                newPaths = addToPaths paths source
+
 
 main = do
     edges <- readGraph "input.txt"
